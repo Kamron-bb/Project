dde1caea659926c758285bd7f6f3cbaec111d555
Kamron-bb <kamolovkamron34@gmail.com>
2024-12-09 11:47:18 +0500

    Update tg_bot.py
    version 2

--- tg_bot.py
+++ tg_bot.py
@@ -23,3 +23,25 @@ def get_eskiz_token(email,password)
         return response.json().get('data',{}).get("token")
     if response.json()['data'] is not None:
         pass
+
+
+@dp.message()
+async def handle_message(message: types.Message):
+    user_id = message.from_user.id
+    if user_id not in user_data or message.text == '/start':
+        await start(message)
+    elif 'phone' not in user_data[user_id]:
+        await sent_code(message)
+    elif 'status' not in user_data[user_id]:
+        await check_code(message)
+    elif 'location' not in user_data[user_id]:
+        await save_address(message)
+    elif 'kategoriyalar'   in user_data[user_id]['keyboard']:
+        await show_menu(message)
+    elif 'tovarlar'  in user_data[user_id]['keyboard']:
+        await show_item(message)
+    elif 'tanlangan_tovar' in user_data[user_id]['keyboard']:
+        await select_item(message)
+
+
+
